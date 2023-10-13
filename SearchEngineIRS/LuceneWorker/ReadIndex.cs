8713b6a09018d18be1f01103a8912e6e231702dc
Bishwadeep Sikder <rahulsikder223@gmail.com>
2023-10-13 03:02:44 -0400

    Feature 1: Highlighting Searched Keywords

--- SearchEngineIRS/LuceneWorker/ReadIndex.cs
+++ SearchEngineIRS/LuceneWorker/ReadIndex.cs
@@ -3,8 +3,10 @@ using Lucene.Net.Documents;
 using Lucene.Net.Index;
 using Lucene.Net.QueryParsers.Classic;
 using Lucene.Net.Search;
+using Lucene.Net.Search.Highlight;
 using Lucene.Net.Store;
 using Lucene.Net.Util;
+using LuceneWorker.DTOs;
 using LuceneDirectory = Lucene.Net.Store.Directory;
 
 namespace LuceneWorker
@@ -13,7 +15,7 @@ namespace LuceneWorker
     {
         const LuceneVersion luceneVersion = LuceneVersion.LUCENE_48;
 
-        public List<string> Read(string searchQuery)
+        public List<ExportDTO> Read(string searchQuery)
         {
             string indexDocumentName = "data_index";
             string indexDocumentPath = Path.Combine(Environment.CurrentDirectory, indexDocumentName);
@@ -21,12 +23,17 @@ namespace LuceneWorker
             LuceneDirectory directory = FSDirectory.Open(indexDocumentPath);
             IndexReader reader = DirectoryReader.Open(directory);
             IndexSearcher searcher = new IndexSearcher(reader);
-            QueryParser queryParser = new QueryParser(luceneVersion, "content", new StandardAnalyzer(luceneVersion));
+            var analyzer = new StandardAnalyzer(luceneVersion);
+            QueryParser queryParser = new QueryParser(luceneVersion, "content", analyzer);
             Query query = queryParser.Parse(searchQuery);
             TopDocs topDocs = searcher.Search(query, n: 10);
 
+            SimpleHTMLFormatter formatter = new SimpleHTMLFormatter("<b>", "</b>");
+            Highlighter highlighter = new Highlighter(formatter, new QueryScorer(query));
+
             int numMatchingDocs = topDocs.TotalHits;
-            List<string> output = new List<string> { "Total Matching Results: " + topDocs.TotalHits };
+            List<ExportDTO> output = new List<ExportDTO>();
+            //List<string> output = new List<string> { "Total Matching Results: " + topDocs.TotalHits };
 
             if (numMatchingDocs > 0)
             {
@@ -34,7 +41,19 @@ namespace LuceneWorker
                 {
                     Document resultDoc = searcher.Doc(doc.Doc);
                     string content = resultDoc.Get("content");
-                    output.Add("Result: " + content);
+                    using var firstLine = new StringReader(content);
+                    string title = firstLine.ReadLine() ?? string.Empty;
+
+                    string highlightedContent = highlighter.GetBestFragment(analyzer, "content", content);
+
+                    ExportDTO exportObject = new ExportDTO
+                    {
+                        PaperName = title,
+                        Description = highlightedContent,
+                        Score = doc.Score
+                    };
+
+                    output.Add(exportObject);
                 }
             }
 
